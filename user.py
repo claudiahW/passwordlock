842ffd581106ec1181117ca96c6a2e64e69b4171
claudiahW <murungiclaud@gmail.com>
2021-10-25 01:20:58 +0300

    initialize the contact array

--- user.py
+++ user.py
@@ -3,4 +3,13 @@ class User:
     Creates new user instances.
     """
     userslist=[]
-     
+    def __init__(self,firstname,lastname,username,password):
+        """
+        __init__ defines the properties of self.
+        Args:
+        firstname: User's first name
+        lastname: User's last name
+        username: New username
+        password: New password
+        """
+
