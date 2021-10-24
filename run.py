c1db2ae940083b4d4538dc94be025c8b6e360727
claudiahW <murungiclaud@gmail.com>
2021-10-25 01:52:24 +0300

    add create account functions

--- run.py
+++ run.py
@@ -10,3 +10,13 @@ def delete_user(user):
     user.delete_user()
 def find_user(number):
     return User.find_by_number(number)
+def display_users():
+    return User.display_users()
+def create_account(accountusername,accountname,accountpassword):
+    newaccount= Details(accountusername,accountname,accountpassword)
+    return newaccount
+def save_account(user):
+    user.save_account()
+def delete_account(user):
+    user.delete_account()
+def find_account(number):
