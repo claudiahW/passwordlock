1773f867bd81b0f5d3b14136810dd9b6a4f5ab7f
claudiahW <murungiclaud@gmail.com>
2021-10-25 01:50:34 +0300

    add more properties to the user function

--- run.py
+++ run.py
@@ -4,3 +4,9 @@ from user import Details
 def create_user(firstname,lastname,username,userpassword):
     newuser= User(firstname,lastname,username,userpassword)
     return newuser
+def save_user(user):
+    user.save_user()
+def delete_user(user):
+    user.delete_user()
+def find_user(number):
+    return User.find_by_number(number)
