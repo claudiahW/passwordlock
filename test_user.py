7f896bb4c5a77ff9f53d115b260dfbf420f1dd43
claudiahW <murungiclaud@gmail.com>
2021-10-25 01:33:59 +0300

    setup self method

--- test_user.py
+++ test_user.py
@@ -9,4 +9,9 @@ class TestUser(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
-    
+def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user =user("Claudia","1234") # create user object
+   
